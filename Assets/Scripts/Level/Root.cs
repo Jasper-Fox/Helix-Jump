dc3b038970176bb71a976bd91b4bd360f652d2f3
Jasper-Fox <azimo74@bk.ru>
2024-06-08 15:13:49 +0300

    Apd LevelGenerator
    Correct Root Length

---
+++ Assets/Scripts/Level/Root.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    private const float t1 = 2/3f;
+    private const float t2 = 0.8f;
+    
+    public LevelGenetator LevelGenetator;
+    
+    void Start()
+    {
+        LevelLengthToRootLength();
+    }
+
+    private void LevelLengthToRootLength()
+    {
+        Vector3 cylinderScale = transform.localScale;
+        cylinderScale.y = LevelGenetator.levelLenght * t1 + t2;
+        transform.localScale = cylinderScale;
+    }
+}
